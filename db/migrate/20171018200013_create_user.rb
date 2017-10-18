11475a214186f1330fd5722d3246654c119de9c3
Christine Tran <christinetran825@gmail.com>
2017-10-18 20:00:24 +0000

    automatically backed up by learn

--- db/migrate/20171018200013_create_user.rb
+++ db/migrate/20171018200013_create_user.rb
@@ -1,4 +1,9 @@
 class CreateUser < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.strings :username
+      t.strings :password
+      t.integers :balance
+    end
   end
 end
