d2fab58ee1ef8bd4849f19d42ebd6cdc9dc548ba
Christine Tran <christinetran825@gmail.com>
2017-10-18 20:33:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"])
-    @user.save
     if @user != nil && @user["password"] == params["password"]
       session[:user_id] = @user["id"]
       redirect to '/account'
