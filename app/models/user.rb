683dccc5801312ff7cca69b40f48ed77234bd85f
Christine Tran <christinetran825@gmail.com>
2017-10-18 19:56:24 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,2 @@
-class User < Application::Base
-
-  # create_table :users do |t|
-  #   t.strings :username
-  #   t.strings :password
-  #   t.integers :balance
-  # end
-  # 
+class User < Application::Base 
 end
