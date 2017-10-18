57fb9cea1146ae43479967175bdd232e6770c833
Christine Tran <christinetran825@gmail.com>
2017-10-18 20:08:15 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,7 @@
 class Helpers
 
   def self.current_user(session)
-    @user = User.create(username: params[:username], password: params[:password], balance: params[:balance])
-    session[:id] = @user
+    @user = User.find(session[:id])
   end
 
   def self.is_logged_in?
